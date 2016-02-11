02b9962b37b0cf3219701114690e18c179cb511a
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-02-11 18:33:50 +0900

    dump date-accesscount-map tsv file

--- amari.py
+++ amari.py
@@ -23,13 +23,19 @@ def parse_access_log(filepath):
 def filter_access_to_not_textual_contents(logmap):
     return [x for x in logmap if not re.search('(.ico|js|css)$', x['request_uri'])]
 
+from datetime import datetime as dt
 def create_accesscount_by_time_map(logmaps):
     d = {}
     for logmap in logmaps:
-        key = logmap['time_local']
+        key = dt.strptime(logmap['time_local'], "%d/%b/%Y").date()
         d[key]=d.get(key, 0) + 1
-    return d
+    return sorted(d.items(),key=lambda e:e[0])
 
 logmaps = parse_access_log('access.log')
 
-print (create_accesscount_by_time_map(filter_access_to_not_textual_contents(logmaps)))
+date_accesscount_map = create_accesscount_by_time_map(
+  filter_access_to_not_textual_contents(logmaps))
+
+print('date\tclose')
+for date_count in date_accesscount_map:
+    print(date_count[0].strftime('%d-%b-%y') + '\t' + str(date_count[1]))
